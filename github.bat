ecd7e588ec2c7f98ce78f0eaab3be75748e952d6
jktai123 <jktai123@gmail.com>
2022-02-03 12:23:10 +0800

    'Father'

--- github.bat
+++ github.bat
@@ -3,6 +3,7 @@ git config --global --unset user.email
 Rem 進入專案資料夾，單獨設定每個repo 使用者名稱/郵箱
 git config user.email "jktai123@gmail.com"
 git config user.name "jktai123"
+git pull
 git add -A .
 git commit -m %1
 git push origin master
