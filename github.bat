6e532067da11884c876894170bb194203a1f5d1e
jktai123 <jktai123@gmail.com>
2022-02-09 06:54:07 +0800

    'gbat'

--- github.bat
+++ github.bat
@@ -5,7 +5,7 @@ Rem 進入專案資料夾，單獨設定每個repo 使用者名稱/郵箱
 
 git config user.email "jktai123@gmail.com"
 git config user.name "jktai123"
-git push origin master
+git pull origin master
 
 git add -A .
 git commit -m %1
