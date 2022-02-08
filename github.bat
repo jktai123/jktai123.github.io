8ab8857d2fd4a7d532704278233066c80f4b2e6d
jktai123 <jktai123@gmail.com>
2022-02-09 04:32:37 +0800

    'Father'

--- github.bat
+++ github.bat
@@ -1,8 +1,10 @@
-git config --global --unset user.name
-git config --global --unset user.email
+
+REM git config --global --unset user.name
+REM git config --global --unset user.email
 Rem 進入專案資料夾，單獨設定每個repo 使用者名稱/郵箱
-git config user.email "jktai123@gmail.com"
-git config user.name "jktai123"
+Rem git config user.email "jktai123@gmail.com"
+Rem git config user.name "jktai123"
+
 git add -A .
 git commit -m %1
 git push origin master
